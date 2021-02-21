acb4914eddf964688fd33ad20fae473981d0e4d8
André Formento <andreformento.sc@gmail.com>
2021-02-21 13:25:36 -0300

    keep state of database

--- censo/settings.py
+++ censo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'censo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/sqlite/db.sqlite3',
     }
 }
 
--- formulario/management/commands/read_data.py
+++ formulario/management/commands/read_data.py
@@ -11,26 +11,38 @@ class Command(BaseCommand):
             type=str,
             help='Place the path for the file with data from municipalities.',
         )
+        parser.add_argument(
+            '--prevent_duplicates',
+            type=bool,
+            help='Validate if database it is empty before execution. Prevent duplicates',
+        )
 
     def handle(self, *args, **options):
         if options['file']:
-            with open(options['file'], 'r') as f:
-                cities = csv.DictReader(f)
-                cities_list = []
+            total_cities_in_database = Municipio.objects.count()
+            execute_dataimport = (not bool(options['prevent_duplicates'])) or total_cities_in_database == 0
 
-                for city in cities:
-                    kwargs = {
-                        'ibge': int(city['IBGE']),
-                        'ibge7': int(city['IBGE7']),
-                        'uf': city['UF'],
-                        'municipio': city['Município'],
-                        'regiao': city['Região'],
-                        'populacao_2020': None if city['População 2020'] == '' else city['População 2020'],
-                        'capital': city['Capital'] == 'Capital',
-                    }
-                    cities_list.append(Municipio(**kwargs))
+            if execute_dataimport:
+                with open(options['file'], 'r') as f:
+                    cities = csv.DictReader(f)
+                    cities_list = []
 
-                Municipio.objects.bulk_create(cities_list)
-            self.stdout.write(self.style.SUCCESS('Successfully read data'))
+                    for city in cities:
+                        kwargs = {
+                            'ibge': int(city['IBGE']),
+                            'ibge7': int(city['IBGE7']),
+                            'uf': city['UF'],
+                            'municipio': city['Município'],
+                            'regiao': city['Região'],
+                            'populacao_2020': None if city['População 2020'] == '' else city['População 2020'],
+                            'capital': city['Capital'] == 'Capital',
+                        }
+                        cities_list.append(Municipio(**kwargs))
+
+                    Municipio.objects.bulk_create(cities_list)
+                total_cities_read = Municipio.objects.count()
+                self.stdout.write(self.style.SUCCESS(f'Successfully read data with {total_cities_read} cities'))
+            else:
+                self.stdout.write(self.style.SUCCESS(f'Skipped read because database already have data with {total_cities_in_database} cities'))
         else:
             raise CommandError('Enter the file path: python manage.py read_data --file data.csv')
